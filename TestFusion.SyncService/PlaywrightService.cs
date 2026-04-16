eec4ecdf0ecf92112cd5a317c843cddd638384f6
jaboi313 <hollanderkelvin@gmail.com>
2026-04-16 10:58:11 +0200

    Added start Task for running playwright functions

---
+++ TestFusion.SyncService/PlaywrightService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Playwright;
+
+namespace TestFusion.SyncService
+{
+    public class PlaywrightService
+    { 
+        public static async Task Run() 
+        { 
+            using var playwright = await Playwright.CreateAsync(); 
+            var browser = await playwright.Chromium.LaunchAsync(new() 
+            { 
+                Headless = false // Only for testing
+            });
+
+            var page = await browser.NewPageAsync(); 
+            await GoToSite(page);
+            await Task.Delay(10000); // Only for testing
+            await browser.CloseAsync();
+        }
+        private static async Task GoToSite(IPage page)
+        {
+            await page.GotoAsync("https://example.com");
+        }
+    }
+}
