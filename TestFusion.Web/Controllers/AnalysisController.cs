a199de635da7797b3bae3112d889d44967a8a055
jaboi313 <hollanderkelvin@gmail.com>
2026-04-19 19:17:42 +0200

    Added analysis page and started on overview screen

---
+++ TestFusion.Web/Controllers/AnalysisController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestFusion.Web.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Generate()
+        {
+            return View();
+        }
+    }
+}
