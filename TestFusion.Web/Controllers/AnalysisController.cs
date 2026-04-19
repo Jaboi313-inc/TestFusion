61889854149a096cc38df7dd1d2ac2c13a2f4b9a
jaboi313 <hollanderkelvin@gmail.com>
2026-04-19 20:23:56 +0200

    Fixed style in consistance. Added ability to pass on data to generation page

--- TestFusion.Web/Controllers/AnalysisController.cs
+++ TestFusion.Web/Controllers/AnalysisController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TestFusion.Web.Models;
 
 namespace TestFusion.Web.Controllers
 {
@@ -7,12 +8,25 @@ namespace TestFusion.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            var items = new List<TestListItemModel>
+            {
+                new TestListItemModel { Id = "1", PartNumber = "001", PartBrand = "Bosch", PartType = "X1", DateTime = DateTime.Now },
+                new TestListItemModel { Id = "2", PartNumber = "002", PartBrand = "Delphi", PartType = "Y2", DateTime = DateTime.Now },
+                new TestListItemModel { Id = "3", PartNumber = "003", PartBrand = "Denso", PartType = "Z3", DateTime = DateTime.Now }
+            };
 
-        public IActionResult Generate()
+            return View(items);
+        }        
+        
+        [HttpPost]
+        public IActionResult Generate(List<string> selectedIds)
         {
-            return View();
+            var model = new GeneratedModel
+            {
+                SelectedIds = selectedIds
+            };
+
+            return View(model);
         }
     }
 }
---
+++ TestFusion.Web/Models/GeneratedModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TestFusion.Web.Models
+{
+    public class GeneratedModel
+    {
+        public List<string> SelectedIds { get; set; }
+    }
+}
