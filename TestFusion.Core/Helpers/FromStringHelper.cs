8eb7a2538616f101232dfbb094577aa2cbde05ec
jaboi313 <hollanderkelvin@gmail.com>
2026-05-21 21:46:18 +0200

    Changed types from int's and decimals inside strings to just int's and decimals. Furthermore fixed results parsing not retrieving from the right tank_position

---
+++ TestFusion.Core/Helpers/FromStringHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TestFusion.Core.Helpers
+{
+    public static class FromStringHelper
+    {
+        public static int ToInt(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return 0;
+
+            return int.TryParse(value, out var result)
+                ? result
+                : 0;
+        }
+
+        public static decimal ToDecimal(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return 0;
+
+            return decimal.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result)
+                ? result
+                : 0;
+        }
+    }
+}
--- TestFusion.Core/Models/TestResult/SubTestModel.cs
+++ TestFusion.Core/Models/TestResult/SubTestModel.cs
@@ -7,15 +7,15 @@ namespace TestFusion.Core.Models.TestResult
         public string TankName { get; set; } = default!;
         public int TankPosition { get; set; } = default!;
 
-        public string Max { get; set; } = default!;
-        public string Min { get; set; } = default!;
+        public decimal Max { get; set; } = default!;
+        public decimal Min { get; set; } = default!;
 
-        public string BlueMax { get; set; } = default!;
-        public string BlueMin { get; set; } = default!;
-        public string YellowMax { get; set; } = default!;
-        public string YellowMin { get; set; } = default!;
+        public decimal BlueMax { get; set; } = default!;
+        public decimal BlueMin { get; set; } = default!;
+        public decimal YellowMax { get; set; } = default!;
+        public decimal YellowMin { get; set; } = default!;
 
-        public string TolBlue { get; set; } = default!;
+        public decimal TolBlue { get; set; } = default!;
 
         public string ProcentMax { get; set; } = default!;
         public string ProcentMin { get; set; } = default!;
@@ -24,7 +24,7 @@ namespace TestFusion.Core.Models.TestResult
         public string ResultMax { get; set; } = default!;
         public string ResultMin { get; set; } = default!;
         public string ResultAverage { get; set; } = default!;
-        public string ResultColor { get; set; } = default!;
+        public int ResultColor { get; set; } = default!;
 
         public List<decimal> Results { get; set; } = new();
 
