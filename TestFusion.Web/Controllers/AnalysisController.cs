e503c03aa5ac6e03f64587d4f800e2c97b4d07d5
jaboi313 <hollanderkelvin@gmail.com>
2026-05-21 21:44:16 +0200

    Fixed namespace of models

--- TestFusion.Web/Controllers/AnalysisController.cs
+++ TestFusion.Web/Controllers/AnalysisController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using TestFusion.Core.Interfaces;
-using TestFusion.Core.Models;
+using TestFusion.Core.Models.WebModels;
 using TestFusion.Data;
 using Microsoft.EntityFrameworkCore;
 
