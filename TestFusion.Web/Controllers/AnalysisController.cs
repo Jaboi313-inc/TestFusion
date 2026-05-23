6baf2c6d2a91f6279cd802937dcd2bd8d98e1dd4
jaboi313 <hollanderkelvin@gmail.com>
2026-05-23 14:27:15 +0200

    Removed comments

--- TestFusion.Web/Controllers/AnalysisController.cs
+++ TestFusion.Web/Controllers/AnalysisController.cs
@@ -10,13 +10,11 @@ namespace TestFusion.Web.Controllers
 {
     public class AnalysisController : Controller
     {
-        private readonly IPlaywright _playwrightInterface;
         private readonly AppDbContext _db;
         private readonly ISyncService _sync;
 
-        public AnalysisController(IPlaywright playwrightInterface, AppDbContext db, ISyncService sync)
+        public AnalysisController(AppDbContext db, ISyncService sync)
         {
-            _playwrightInterface = playwrightInterface;
             _db = db;
             _sync = sync;
         }
@@ -37,30 +35,6 @@ namespace TestFusion.Web.Controllers
 
             return View(items);
         }
-
-            /*
-            var debugJson = JsonSerializer.Serialize(
-            item,
-            new JsonSerializerOptions
-            {
-                WriteIndented = true
-            });
-
-            System.IO.File.WriteAllText(
-                @"D:\Progammeren\repos\TestFusion\TestFusion.Web\mapped.json",
-                debugJson
-            );
-
-            return View(new List<TestListItemModel> { item });
-            */
-
-            /*
-            var items = await Task.WhenAll(
-                ids.Select(id => _playwrightInterface.GetDataForId(id))
-            );
-
-            return View(items.ToList());
-            */
         
         [HttpPost]
         public IActionResult Generate(List<string> selectedIds)
