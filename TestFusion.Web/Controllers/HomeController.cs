16d4237c517949c2ee80c5035bd6a6c6abed0c4c
jaboi313 <hollanderkelvin@gmail.com>
2026-09-11 01:22:52 +0200

    Added option to select amount of test that are shown

--- TestFusion.Web/Controllers/HomeController.cs
+++ TestFusion.Web/Controllers/HomeController.cs
@@ -18,12 +18,61 @@ namespace TestFusion.Web.Controllers
             _sync = sync;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? resultLimit = null)
         {
-            var items = await _db.TestItems
-                .OrderByDescending(x => x.DateTime)
-                .Take(50)
-                .ToListAsync();
+            const int defaultLimit = 50;
+            const string cookieName = "TestFusion.ResultLimit";
+
+            var allowedLimits = new[] { 25, 50, 100, 0 };
+
+            int selectedLimit;
+
+            if (resultLimit.HasValue &&
+                allowedLimits.Contains(resultLimit.Value))
+            {
+                selectedLimit = resultLimit.Value;
+
+                Response.Cookies.Append(
+                    cookieName,
+                    selectedLimit.ToString(),
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddYears(1),
+                        IsEssential = true,
+                        HttpOnly = true,
+                        SameSite = SameSiteMode.Lax,
+                        Secure = Request.IsHttps,
+                        Path = "/"
+                    });
+            }
+            else if (
+                Request.Cookies.TryGetValue(cookieName, out var savedValue) &&
+                int.TryParse(savedValue, out var savedLimit) &&
+                allowedLimits.Contains(savedLimit))
+            {
+                selectedLimit = savedLimit;
+            }
+            else
+            {
+                selectedLimit = defaultLimit;
+            }
+
+
+            var totalCount =
+                await _db.TestItems.CountAsync();
+
+
+            var query = _db.TestItems
+                .OrderByDescending(x => x.DateTime);
+
+
+            var items = selectedLimit == 0
+                ? await query.ToListAsync()
+                : await query.Take(selectedLimit).ToListAsync();
+
+
+            ViewBag.ResultLimit = selectedLimit;
+            ViewBag.TotalCount = totalCount;
 
             return View(items);
         }
